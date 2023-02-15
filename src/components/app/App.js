8f15ae5594f979bacf2fcaab53b01f92b5bda03a
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-02-15 20:23:38 +0700

    create pages and add links from react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Route, Routes, Router } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -6,6 +7,13 @@ import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from "../../resources/img/vision.png";
 import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+import {
+  MainPage,
+  ComicsPage,
+  Page404,
+  SingleComicPage,
+} from "../../pages/index";
 
 const App = () => {
   const [selectedChar, setChar] = useState(null);
@@ -18,17 +26,22 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        {/* <RandomChar/>
-                    
-                    
-                     <div className="char__content">
-                        <CharList onCharSelected={onCharSelected}/>
-                         <ErrorBoundary>
-                           <CharInfo charId={selectedChar}/>
-                        </ErrorBoundary> 
-                    </div> 
-                    <img className="bg-decoration" src={decoration} alt="vision"/> */}
-        <ComicsList />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/comics" element={<ComicsPage />} />
+          <Route path="/comics/:comicId" element={<SingleComicPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+        {/* <RandomChar />
+
+          <div className="char__content">
+            <CharList onCharSelected={onCharSelected} />
+            <ErrorBoundary>
+              <CharInfo charId={selectedChar} />
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+          <ComicsList /> */}
       </main>
     </div>
   );
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,22 +1,34 @@
-import './appHeader.scss';
+import "./appHeader.scss";
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
-    return (
-        <header className="app__header">
-            <h1 className="app__title">
-                <a href="#">
-                    <span>Marvel</span> information portal
-                </a>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    /
-                    <li><a href="#">Comics</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="app__header">
+      <Link to="/">
+        <h1 className="app__title">
+          <a href="#">
+            <span>Marvel</span> information portal
+          </a>
+        </h1>
+      </Link>
+
+      <nav className="app__menu">
+        <ul>
+          <Link to="/">
+            <li>
+              <a href="#">Characters</a>
+            </li>
+          </Link>
+          /
+          <Link to="/comics">
+            <li>
+              <a href="#">Comics</a>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default AppHeader;
